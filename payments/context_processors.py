beb46de84e6a59248b102eca3f4b86176228dadb
lkbrian <lkbrian742@gmail.com>
2025-10-07 19:55:01 +0300

    ref: main update

--- payments/context_processors.py
+++ payments/context_processors.py
@@ -4,4 +4,4 @@ from .models import Paywall
 def payment_context(request):
     """Makes the first Paywall record available globally."""
     payment = Paywall.objects.first()
-    return {"payment": payment}
+    return {"paywall": payment}
